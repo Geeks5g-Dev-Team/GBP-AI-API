3fbc13455e953046b3db610992f385407b29ffb7
Brandon Velásquez Osorio <brandon@geeks5g.com>
2025-04-02 11:07:18 -0500

    fix(generator): change endpoint response to generate images to url string

--- src/presentation/controllers/generator.controller.ts
+++ src/presentation/controllers/generator.controller.ts
@@ -9,14 +9,15 @@ export class GeneratorController {
   constructor(private readonly generateImageOfServiceUseCase: GenerateImageOfServiceUseCase) {}
 
   @Post('image-of-service')
-  async generateImageOfService(@Body() bodyDto: GenerateImageOfServiceDto, @Request() req: any): Promise<StreamableFile> {
-    const [imagePath] = await this.generateImageOfServiceUseCase.execute(bodyDto);
+  async generateImageOfService(@Body() bodyDto: GenerateImageOfServiceDto, @Request() req: any): Promise<{ url: string }> {
+    const [, uploadUrl] = await this.generateImageOfServiceUseCase.execute(bodyDto);
     // split by \
-    const imageName = imagePath.split('\\').pop();
-    const file = createReadStream(join(process.cwd(), `downloads/images/${imageName}`));
-    return new StreamableFile(file, {
-      disposition: `attachment; filename=${bodyDto.companyName.replace(' ', '')}_${bodyDto.businessType.replace(' ', '')}.jpg`,
-      type: 'image/jpg',
-    });
+    // const imageName = imagePath.split('\\').pop();
+    // const file = createReadStream(join(process.cwd(), `downloads/images/${imageName}`));
+    // return new StreamableFile(file, {
+    //   disposition: `attachment; filename=${bodyDto.companyName.replace(' ', '')}_${bodyDto.businessType.replace(' ', '')}.jpg`,
+    //   type: 'image/jpg',
+    // });
+    return { url: uploadUrl };
   }
 }
